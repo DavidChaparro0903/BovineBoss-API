2f11f1f5a6b2412d047ea909776caf256498a793
David Chaparro <david.chaparro01@uptc.edu.co>
2023-04-24 12:58:42 -0500

    cambios en dtos

---
+++ BovineBoss-API/Models/Dtos/RazaResDto.cs
@@ -0,0 +1,13 @@
+﻿namespace BovineBoss_API.Models.Dtos
+{
+    public class RazaResDto
+    {
+        public int IdRaza { get; set; }
+
+        public string NombreRaza { get; set; } = null!;
+
+        public int PorcentajeRaza { get; set; }
+
+
+    }
+}
